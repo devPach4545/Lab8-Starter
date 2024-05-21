142c5276d62329e48da8fe3ca77991a025e4a868
Dhaivat <dhaivatpachchigar@gmail.com>
2024-05-21 14:28:24 -0700

    Co-authored-by: Andrew Smithwick <WishingWell13@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lab8-Starter
+Dhaivat Pachchigar
+Andrew Smithwick
+
+
+TODO:
+
+In your README.md, please add a short paragraph answering the question: How are graceful degradation and service workers related? (Answer this after doing the part below)
+
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,8 +45,15 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if('serviceWorker' in navigator){
+    console.log("It is supported");
+  }
+  else{
+    console.log("Not supported");
+  }
+
   // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
+   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
   // B4. TODO - Once the service worker has been successfully registered, console
@@ -55,6 +62,12 @@ function initializeServiceWorker() {
   //            log that it has failed.
   // STEPS B6 ONWARDS WILL BE IN /sw.js
 }
+  window.addEventListener('load', function(){
+    this.navigator.serviceWorker.register("./sw.js").then(
+      console.log("Register ./sw.js success!")
+    ).catch("Register ./sw.js failed :(");
+  });
+ 
 
 /**
  * Reads 'recipes' from localStorage and returns an array of
@@ -68,38 +81,85 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  const localRecipes = localStorage.getItem('recipes');
+  if(localRecipes !== null){
+    return JSON.parse(localRecipes);
+  }
+
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  let fetchedRecipes = [];
+
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
-  /**************************/
-  // A4-A11 will all be *inside* the callback function we passed to the Promise
-  // we're returning
-  /**************************/
-  // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
-  //            declared above
-  // A5. TODO - Since we are going to be dealing with asynchronous code, create
-  //            a try / catch block. A6-A9 will be in the try portion, A10-A11
-  //            will be in the catch portion.
-  // A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
-  //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
-  //            you must either use "await fetch(...)" or "fetch.then(...)". This
-  //            function is using the async keyword so we recommend "await"
-  // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
-  //            NOTE: .json() is ALSO asynchronous, so you will need to use
-  //            "await" again
-  // A8. TODO - Add the new recipe to the recipes array
-  // A9. TODO - Check to see if you have finished retrieving all of the recipes,
-  //            if you have, then save the recipes to storage using the function
-  //            we have provided. Then, pass the recipes array to the Promise's
-  //            resolve() method.
-  // A10. TODO - Log any errors from catch using console.error
-  // A11. TODO - Pass any errors to the Promise's reject() function
+
+  //Can mark () => functions as async like this
+  return new Promise(async (resolve, reject) => {
+    
+
+  
+    /**************************/
+    // A4-A11 will all be *inside* the callback function we passed to the Promise
+    // we're returning
+    /**************************/
+    // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
+    //            declared above
+
+    RECIPE_URLS.forEach(async (currVal, ind, arr) => {
+      try{
+
+        // A5. TODO - Since we are going to be dealing with asynchronous code, create
+        //            a try / catch block. A6-A9 will be in the try portion, A10-A11
+        //            will be in the catch portion.
+        // wouldn't use an await in a promise
+        // when you call a promise, would use an await
+
+
+        // A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
+        //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
+        //            you must either use "await fetch(...)" or "fetch.then(...)". This
+        //            function is using the async keyword so we recommend "await"
+        
+        const result = await fetch(currVal)
+        
+
+       // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
+        //            NOTE: .json() is ALSO asynchronous, so you will need to use
+        //            "await" again
+
+        const newRecipe = await result.json();
+
+        // A8. TODO - Add the new recipe to the recipes array
+        fetchedRecipes.push(newRecipe);
+
+        // A9. TODO - Check to see if you have finished retrieving all of the recipes,
+        //            if you have, then save the recipes to storage using the function
+        //            we have provided. Then, pass the recipes array to the Promise's
+        //            resolve() method.
+        
+        if(fetchedRecipes.length == arr.length){
+          console.log("End ind " + ind + " with length " + arr.length);
+          console.log(fetchedRecipes);
+          saveRecipesToStorage(fetchedRecipes);
+          resolve(fetchedRecipes);
+        }else{
+          console.log("At ind " + ind + " with length " + arr.length);
+        }
+        
+      }catch(error){
+        // A10. TODO - Log any errors from catch using console.error
+        // A11. TODO - Pass any errors to the Promise's reject() function
+        console.error(error);
+        reject(error);
+      }
+
+    });
+  });
 }
 
 /**
